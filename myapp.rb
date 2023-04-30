ce883735f7f6349727babd06258f880519775fed
PoliceHinter <danila.stepanidenko@mail.ru>
2023-04-30 15:24:23 +0300

    delete inventory from ruby

--- myapp.rb
+++ myapp.rb
@@ -4,13 +4,13 @@ require 'ansible'
 
 post '/payload' do
   # Получаем json от github webhook
-  push = JSON.parse(request.body.read)
+  #push = JSON.parse(request.body.read)
   # Выводим json
-  puts "I got some JSON: #{push.inspect}"
+  #puts "I got some JSON: #{push.inspect}"
 
   # Задаем файл инвентаризации и путь к плэйбуку
-  inventory = Ansible::Inventory::FilePathInventory.new('./hosts')
-  playbook = Ansible::Playbook::Play.new('./ansible_deploy.yml', inventory)
+  #inventory = Ansible::Inventory::FilePathInventory.new('./hosts')
+  playbook = Ansible::Playbook::Play.new('./ansible_deploy.yml')
   
   # Переменные для ансибл при необходимости
   #playbook.exta_vars = { 'var1' => 'value1', '' => '' }
