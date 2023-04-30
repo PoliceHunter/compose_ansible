e25c449520f2677c050d4581735e979d4ceb6f56
PoliceHinter <danila.stepanidenko@mail.ru>
2023-04-30 15:17:54 +0300

    try to run ansible

--- myapp.rb
+++ myapp.rb
@@ -1,7 +1,25 @@
 require 'sinatra'
 require 'json'
+require 'ansible'
 
 post '/payload' do
+  # Получаем json от github webhook
   push = JSON.parse(request.body.read)
+  # Выводим json
   puts "I got some JSON: #{push.inspect}"
+
+  # Задаем файл инвентаризации и путь к плэйбуку
+  inventory = Ansible::Inventory::FilePathInventory.new('./hosts')
+  playbook = Ansible::Playbook::Play.new('./ansible_deploy.yml')
+  
+  # Переменные для ансибл при необходимости
+  #playbook.exta_vars = { 'var1' => 'value1', '' => '' }
+  #
+  #
+  #Run ansible 
+  result = playbook.run
+  puts result.to_json
+
+  # Возвращаем результаты Ansible в качестве ответа на запрос
+  result.to_json
 end
--- payload.php
+++
@@ -1,6 +0,0 @@
-<?php
-if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
-  echo shell_exec('cd /home/danila/compose_ansible/ && git pull && ansible-playbook ansible_deploy.yml');
-}
-?>
-
